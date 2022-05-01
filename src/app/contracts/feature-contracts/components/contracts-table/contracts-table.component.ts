2449b44856c5570634fff36684a3142d68283ff4
Moises Leonor <leonormoises27@outlook.com>
2022-04-30 20:20:47 -0600

    Add table actions

--- src/app/contracts/feature-contracts/components/contracts-table/contracts-table.component.ts
+++ src/app/contracts/feature-contracts/components/contracts-table/contracts-table.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Contract } from './../../../data-access/models/contract';
-
+import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'ontop-contracts-table',
   templateUrl: './contracts-table.component.html',
@@ -10,4 +10,11 @@ import { Contract } from './../../../data-access/models/contract';
 })
 export class ContractsTableComponent {
   @Input() contracts$!: Observable<Contract[]>;
+
+  actions: MenuItem[] = [
+    { label: 'Edit', icon: 'pi pi-pencil' },
+    { label: 'Sign', icon: 'pi pi-key' },
+    { label: 'View summary', icon: 'pi pi-file' },
+    { label: 'Delete', icon: 'pi pi-trash' },
+  ];
 }
--- src/app/contracts/feature-contracts/feature-contracts.module.ts
+++ src/app/contracts/feature-contracts/feature-contracts.module.ts
@@ -5,9 +5,15 @@ import { FeatureContractsRoutingModule } from './feature-contracts-routing.modul
 import { ContractsComponent } from './components/contracts/contracts.component';
 import { TableModule } from 'primeng/table';
 import { ContractsTableComponent } from './components/contracts-table/contracts-table.component';
+import { MenuModule } from 'primeng/menu';
 
 @NgModule({
   declarations: [ContractsComponent, ContractsTableComponent],
-  imports: [CommonModule, TableModule, FeatureContractsRoutingModule],
+  imports: [
+    CommonModule,
+    TableModule,
+    FeatureContractsRoutingModule,
+    MenuModule,
+  ],
 })
 export class FeatureContractsModule {}
